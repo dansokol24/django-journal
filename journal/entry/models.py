751c188dfb87976762adaa7557cfb8752743d58f
Dan Sokol <Dansokol.code24@gmail.com>
2022-03-04 23:00:46 -0500

    Redid the creation of the files to include pipenv which will synchonize the environments between devices, I also created the sqlite database for entries which will have a date, header, and content, it will be organized by date

---
+++ journal/entry/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Entry(models.Model):
+    date = models.DateTimeField()
+    header = models.CharField(max_length=70)
+    content = models.TextField()
+    
+    class Meta:
+        ordering = ('date',)
+    
